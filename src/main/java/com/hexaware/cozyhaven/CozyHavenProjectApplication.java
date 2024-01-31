a24a764f0444ef1664abc0a471db8d1011f8b681
haswanth9891 <19bcs071@iiit.ac.in>
2024-01-31 19:38:39 +0530

    First commit

---
+++ src/main/java/com/hexaware/cozyhaven/CozyHavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.cozyhaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyHavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozyHavenProjectApplication.class, args);
+	}
+
+}
